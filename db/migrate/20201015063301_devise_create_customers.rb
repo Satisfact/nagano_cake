f1324202f5aabc191b8bd6d52b77a714bdb0e911
arareosenbei <yohei.1101.o@gmail.com>
2020-10-17 06:08:02 +0000

    model,controller,カラム作成

--- db/migrate/20201015063301_devise_create_customers.rb
+++ db/migrate/20201015063301_devise_create_customers.rb
@@ -32,7 +32,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :last_name, null: false #姓
+      t.string :first_name, null: false #名
+      t.string :last_name_kana, null: false #セイ
+      t.string :first_name_kana, null: false #メイ
+      t.string :postal_code, null: false #郵便番号
+      t.string :address, null: false #住所
+      t.string :telephone_number, null: false #電話番号
+      t.boolean :is_deleted, default: true, null: false # 会員ステータス
       t.timestamps null: false
     end
 
