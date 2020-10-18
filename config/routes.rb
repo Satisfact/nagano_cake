9293215902e614d434f204c87714c491905e86e2
EC2 Default User <ec2-user@ip-172-31-73-55.ec2.internal>
2020-10-18 02:34:31 +0000

    管理者側のitem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ get 'show' => 'items#show'
+ get 'edit' => 'items#edit'
+ get 'new' => 'items#new'
+ get 'index' => 'items#index'
+ get 'top' => 'homes#top'
+ get 'home/about' => 'homes#about', as: 'about'
 end
