8f3490a0617e2cbdd6556e44c75a5fbe175f9674
Hideyo Gotoda <16.hide.13.love@gmail.com>
2020-10-18 04:02:37 +0000

    admin微調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_10_17_064408) do
 
   create_table "admins", force: :cascade do |t|
@@ -39,14 +38,6 @@ ActiveRecord::Schema.define(version: 2020_10_17_064408) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "last_name", null: false
-    t.string "first_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "first_name_kana", null: false
-    t.string "postal_code", null: false
-    t.string "address", null: false
-    t.string "telephone_number", null: false
-    t.boolean "is_deleted", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
