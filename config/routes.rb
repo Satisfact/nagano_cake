a30986829b6e76d27aeeabf8c0bf2c4c09f06214
EC2 Default User <ec2-user@ip-172-31-73-55.ec2.internal>
2020-10-19 06:35:42 +0000

    ルートを誤って消したので修正

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items, only: [:top, :index, :show]
   root 'items#top'
-
+  get 'homes/about' => 'homes#about'
+  get 'edit' => 'customers#edit'
 end
