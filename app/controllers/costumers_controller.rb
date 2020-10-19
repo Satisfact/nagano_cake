9e77e6068c5e398c58258b9d6341268e992c8894
EC2 Default User <ec2-user@ip-172-31-73-55.ec2.internal>
2020-10-19 01:52:46 +0000

    更新

---
+++ app/controllers/costumers_controller.rb
@@ -0,0 +1,30 @@
+class CostumersController < ApplicationController
+  #before_action :authenticate_costmer!
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  
+
+  # ユーザーの退会（論理削除）=> "物理削除"ではないためupdateを使用している。
+  def destroy
+    #is_deletedカラムにフラグを立てる(defaultはfalse)
+
+    #ログアウトさせる
+
+  end
+
+ private
+  def
+  end
+
+  end
+end
