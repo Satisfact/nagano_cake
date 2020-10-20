0faeed6fec275e90d7ac5d104a0e8b9e2e128452
Hideyo Gotoda <16.hide.13.love@gmail.com>
2020-10-20 09:34:56 +0000

    order関連レイアウト、コントローラー追加

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+  #before_action :authenticate!
+  #before_action :customer_is_deleted
+  #注文情報入力
+  def new
+  end
+  
+  def create
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
+ #注文履歴
+  def index
+  end
+
+  def show
+  end
+end
