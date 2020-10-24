4ae7646beb672e820e80c9f4095d2c0ada74c0ad
EC2 Default User <ec2-user@ip-172-31-73-55.ec2.internal>
2020-10-24 11:57:08 +0000

    顧客のregistrationsのコード修正

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -43,7 +43,7 @@ class Customers::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :last_name,:first_name, :last_kana, :first_kana,:postcode,:address,:telephone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :last_name,:first_name, :last_kana, :first_kana,:postal_code,:address,:telephone_number])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
