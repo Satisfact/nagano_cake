46c423b2e1e401b424d4e19f91e4975e9fbb9490
arareosenbei <yohei.1101.o@gmail.com>
2020-10-25 07:44:28 +0000

    追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
-  before_action :authenticate_customer!
   
   protected
   def after_sign_in_path_for(resource)
@@ -12,13 +11,13 @@ class ApplicationController < ActionController::Base
       end
   end
 
-  def after_sign_out_path_for(resource)
-      case resource
-        when Admin
-          new_admin_session_path
-            when Customer
-              root_path
-      end
-  end
+  #def after_sign_out_path_for(resource)
+  #    case resource
+  #      when Admin
+  #        new_admin_session_path
+  #          when Customer
+  #            root_path
+  #    end
+  #end
 
 end
