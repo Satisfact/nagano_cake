73b3d7f386772966ea7a53abd5b86508d4a58d2d
Hideyo Gotoda <16.hide.13.love@gmail.com>
2020-10-26 05:21:06 +0000

    オーダー系パート１

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,11 @@ class Address < ApplicationRecord
     validates :postcode, presence: true
     validates :address, presence: true
     validates :name, presence: true
-    
+
     belongs_to :customer
+    
+    #テーブルのセルをたす
+    def full_address
+        self.postcode + self.address + self.name
+    end
 end
