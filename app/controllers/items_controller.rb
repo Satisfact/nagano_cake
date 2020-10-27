0f982fb5f1fb8662fe847648cb167e788341fe3a
Hideyo Gotoda <16.hide.13.love@gmail.com>
2020-10-27 15:47:31 +0000

    微調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,28 +3,34 @@ class ItemsController < ApplicationController
  def top
   if params[:genre_id].present?
    @sweet_item = Item.where(genre_id: params[:genre_id])
-   @sweet_items = @sweet_item.where(is_active: true).page(params[:page]).per(4)
+   #sample()ランダム
+   @sweet_items = @sweet_item.where(is_active: true).sample(4)
    #@genre = Genre.find(params[:genre_id])
    @genres = Genre.where(is_active: true)
    @title = Genre.find(params[:genre_id]).name
   else
-   @sweet_items = Item.where(is_active: true).page(params[:page]).per(4)
+   @sweet_items = Item.where(is_active: true).sample(4)
    @genres = Genre.where(is_active: true)
    @title = "商品"
   end
  end
+
  def index
   #kaminariつける（不要？）
   #ジャンル検索
   if params[:genre_id].present?
    @sweet_item = Item.where(genre_id: params[:genre_id])
    @sweet_items = @sweet_item.where(is_active: true).page(params[:page]).per(8)
+   #viewのcount用
+   @items =  @sweet_item.where(is_active: true)
    #@genre = Genre.find(params[:genre_id])
    @genres = Genre.where(is_active: true)
    @title = Genre.find(params[:genre_id]).name
   else
    @sweet_items = Item.where(is_active: true).page(params[:page]).per(8)
    @genres = Genre.where(is_active: true)
+   
+   @items =  Item.where(is_active: true)
    @title = "商品"
   end
  end
