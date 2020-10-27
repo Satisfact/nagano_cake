1d10e864fb4452e2ee19c6803eccb7f7dcd2acd8
EC2 Default User <ec2-user@ip-172-31-73-55.ec2.internal>
2020-10-27 02:14:42 +0000

    itemコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,18 @@
 class ItemsController < ApplicationController
 #管理者用別途作成
  def top
-  #アクティブtrueのジャンルを表示
-  @genres = Genre.where(is_active: true)
-  #limit(個数).offset(どこから)
-  @sweet_items = Item.limit(4).offset(0)
+  if params[:genre_id].present?
+   @sweet_item = Item.where(genre_id: params[:genre_id])
+   @sweet_items = @sweet_item.where(is_active: true).page(params[:page]).per(4)
+   #@genre = Genre.find(params[:genre_id])
+   @genres = Genre.where(is_active: true)
+   @title = Genre.find(params[:genre_id]).name
+  else
+   @sweet_items = Item.where(is_active: true).page(params[:page]).per(4)
+   @genres = Genre.where(is_active: true)
+   @title = "商品"
+  end
  end
-
  def index
   #kaminariつける（不要？）
   #ジャンル検索
