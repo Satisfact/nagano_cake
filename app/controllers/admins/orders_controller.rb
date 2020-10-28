e0445b94320835672db89991924e88329279e4cb
EC2 Default User <ec2-user@ip-172-31-31-133.ec2.internal>
2020-10-28 07:39:09 +0000

    o-da-

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -12,12 +12,12 @@ class Admins::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
-      redirect_to admins_orders_path, notice: "注文ステータス更新しました"
+      redirect_to admins_orders_path, notice: "ステータス更新しました"
     end
   end
 
   private
   def order_params
-    params.require(:order).permit(:customer_id, :postcode, :address, :name, :send_cost, :bill, :pay, :ordered_status)
+    params.require(:order).permit(:customer_id, :postcode, :address, :name, :send_cost, :bill, :pay, :order_status)
   end
 end
